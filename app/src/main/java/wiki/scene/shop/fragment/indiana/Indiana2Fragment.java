bd4734d8991c16d622e4d393b0b7241e3dfe3f67
renjunjia3 <376124750@qq.com>
2017-06-29 11:36:03 +0800

    我的基本完成

--- app/src/main/java/wiki/scene/shop/fragment/indiana/Indiana2Fragment.java
+++ app/src/main/java/wiki/scene/shop/fragment/indiana/Indiana2Fragment.java
@@ -11,12 +11,15 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.youth.banner.Banner;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.iwgang.countdownview.CountdownView;
 import wiki.scene.loadmore.PtrClassicFrameLayout;
 import wiki.scene.loadmore.PtrDefaultHandler;
 import wiki.scene.loadmore.PtrFrameLayout;
@@ -29,7 +32,8 @@ import wiki.scene.shop.fragment.indiana.mvpview.IIndianaView;
 import wiki.scene.shop.fragment.indiana.presenter.IndianaPresenter;
 import wiki.scene.shop.itemDecoration.IndianaItemDecoration;
 import wiki.scene.shop.mvp.BaseMainMvpFragment;
-import wiki.scene.shop.widgets.CustomeGridView;
+import wiki.scene.shop.widgets.RatioImageView;
+import wiki.scene.shop.widgets.verticalrollingtextview.VerticalRollingTextView;
 
 /**
  * Case By:
@@ -74,17 +78,10 @@ public class Indiana2Fragment extends BaseMainMvpFragment<IIndianaView, IndianaP
     View linePrice;
     @BindView(R.id.layout_price)
     RelativeLayout layoutPrice;
-    //headerView
-    private TextView textPopular1;
-    private View linePopular1;
-    private TextView textNewest1;
-    private View lineNewest1;
-    private TextView textFastest1;
-    private View lineFastest1;
-    private TextView textPrice1;
-    private View linePrice1;
 
-    private CustomeGridView newestGridView;
+    //headerView
+    IndianaHeaderView indianaHeaderView;
+
     //adapter
     private List<String> list = new ArrayList<>();
     private RecyclerAdapterWithHF mAdapter;
@@ -136,8 +133,8 @@ public class Indiana2Fragment extends BaseMainMvpFragment<IIndianaView, IndianaP
         adapter = new IndianaAdapter(_mActivity, list);
         mAdapter = new RecyclerAdapterWithHF(adapter);
         View headerView = LayoutInflater.from(_mActivity).inflate(R.layout.fragment_indiana_header_view, null);
+        indianaHeaderView = new IndianaHeaderView(headerView);
         mAdapter.addHeader(headerView);
-        initHeaderView(headerView);
         recyclerView.setAdapter(mAdapter);
 
         int width = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
@@ -168,26 +165,7 @@ public class Indiana2Fragment extends BaseMainMvpFragment<IIndianaView, IndianaP
 
             }
         });
-    }
-
-    private void initHeaderView(View headerView) {
-        textPopular1 = (TextView) headerView.findViewById(R.id.text_popular1);
-        textNewest1 = (TextView) headerView.findViewById(R.id.text_newest1);
-        textFastest1 = (TextView) headerView.findViewById(R.id.text_fastest1);
-        textPrice1 = (TextView) headerView.findViewById(R.id.text_price1);
-        linePopular1 = headerView.findViewById(R.id.line_popular1);
-        lineNewest1 = headerView.findViewById(R.id.line_newest1);
-        lineFastest1 = headerView.findViewById(R.id.line_fastest1);
-        linePrice1 = headerView.findViewById(R.id.line_price1);
-        RelativeLayout layoutPopular1 = (RelativeLayout) headerView.findViewById(R.id.layout_popular1);
-        RelativeLayout layoutNewest1 = (RelativeLayout) headerView.findViewById(R.id.layout_newest1);
-        RelativeLayout layoutFastest1 = (RelativeLayout) headerView.findViewById(R.id.layout_fastest1);
-        RelativeLayout layoutPrice1 = (RelativeLayout) headerView.findViewById(R.id.layout_price1);
-
-        layoutPopular1.setOnClickListener(this);
-        layoutNewest1.setOnClickListener(this);
-        layoutFastest1.setOnClickListener(this);
-        layoutPrice1.setOnClickListener(this);
+        indianaHeaderView.bindNewestGoods();
     }
 
     @Override
@@ -247,38 +225,38 @@ public class Indiana2Fragment extends BaseMainMvpFragment<IIndianaView, IndianaP
         textPrice.setTextColor(getResources().getColor(R.color.text_color_title));
         linePrice.setBackgroundColor(getResources().getColor(R.color.transparent));
 
-        textPopular1.setTextColor(getResources().getColor(R.color.text_color_title));
-        linePopular1.setBackgroundColor(getResources().getColor(R.color.transparent));
-        textNewest1.setTextColor(getResources().getColor(R.color.text_color_title));
-        lineNewest1.setBackgroundColor(getResources().getColor(R.color.transparent));
-        textFastest1.setTextColor(getResources().getColor(R.color.text_color_title));
-        lineFastest1.setBackgroundColor(getResources().getColor(R.color.transparent));
-        textPrice1.setTextColor(getResources().getColor(R.color.text_color_title));
-        linePrice1.setBackgroundColor(getResources().getColor(R.color.transparent));
+        indianaHeaderView.textPopular1.setTextColor(getResources().getColor(R.color.text_color_title));
+        indianaHeaderView.linePopular1.setBackgroundColor(getResources().getColor(R.color.transparent));
+        indianaHeaderView.textNewest1.setTextColor(getResources().getColor(R.color.text_color_title));
+        indianaHeaderView.lineNewest1.setBackgroundColor(getResources().getColor(R.color.transparent));
+        indianaHeaderView.textFastest1.setTextColor(getResources().getColor(R.color.text_color_title));
+        indianaHeaderView.lineFastest1.setBackgroundColor(getResources().getColor(R.color.transparent));
+        indianaHeaderView.textPrice1.setTextColor(getResources().getColor(R.color.text_color_title));
+        indianaHeaderView.linePrice1.setBackgroundColor(getResources().getColor(R.color.transparent));
         switch (choosedPosition) {
             case TITLE_BAR_popular:
                 textPopular.setTextColor(getResources().getColor(R.color.colorPrimary));
                 linePopular.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-                textPopular1.setTextColor(getResources().getColor(R.color.colorPrimary));
-                linePopular1.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                indianaHeaderView.textPopular1.setTextColor(getResources().getColor(R.color.colorPrimary));
+                indianaHeaderView.linePopular1.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
                 break;
             case TITLE_BAR_NEWEST:
                 textNewest.setTextColor(getResources().getColor(R.color.colorPrimary));
                 lineNewest.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-                textNewest1.setTextColor(getResources().getColor(R.color.colorPrimary));
-                lineNewest1.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                indianaHeaderView.textNewest1.setTextColor(getResources().getColor(R.color.colorPrimary));
+                indianaHeaderView.lineNewest1.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
                 break;
             case TITLE_BAR_FASTEST:
                 textFastest.setTextColor(getResources().getColor(R.color.colorPrimary));
                 lineFastest.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-                textFastest1.setTextColor(getResources().getColor(R.color.colorPrimary));
-                lineFastest1.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                indianaHeaderView.textFastest1.setTextColor(getResources().getColor(R.color.colorPrimary));
+                indianaHeaderView.lineFastest1.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
                 break;
             case TITLE_BAR_PRICE:
                 textPrice.setTextColor(getResources().getColor(R.color.colorPrimary));
                 linePrice.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-                textPrice1.setTextColor(getResources().getColor(R.color.colorPrimary));
-                linePrice1.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                indianaHeaderView.textPrice1.setTextColor(getResources().getColor(R.color.colorPrimary));
+                indianaHeaderView.linePrice1.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
                 break;
         }
     }
@@ -292,4 +270,120 @@ public class Indiana2Fragment extends BaseMainMvpFragment<IIndianaView, IndianaP
     public void onDestroyView() {
         super.onDestroyView();
     }
+
+    public class IndianaHeaderView {
+        @BindView(R.id.banner)
+        Banner banner;
+        @BindView(R.id.indiana_price_10)
+        TextView indianaPrice10;
+        @BindView(R.id.indiana_fast)
+        TextView indianaFast;
+        @BindView(R.id.indiana_red88)
+        TextView indianaRed88;
+        @BindView(R.id.invite_money)
+        TextView inviteMoney;
+        @BindView(R.id.notice_textView)
+        VerticalRollingTextView noticeTextView;
+        @BindView(R.id.newest_title_image_text)
+        TextView newestTitleImageText;
+        @BindView(R.id.more_newest)
+        TextView moreNewest;
+        @BindView(R.id.newest_goods_image1)
+        RatioImageView newestGoodsImage1;
+        @BindView(R.id.newest_goods_name1)
+        TextView newestGoodsName1;
+        @BindView(R.id.text1)
+        TextView text1;
+        @BindView(R.id.newest_countDownView1)
+        CountdownView newestCountDownView1;
+        @BindView(R.id.newest_goods1)
+        LinearLayout newestGoods1;
+        @BindView(R.id.newest_goods_image2)
+        RatioImageView newestGoodsImage2;
+        @BindView(R.id.newest_goods_name2)
+        TextView newestGoodsName2;
+        @BindView(R.id.text2)
+        TextView text2;
+        @BindView(R.id.newest_countDownView2)
+        CountdownView newestCountDownView2;
+        @BindView(R.id.newest_goods2)
+        LinearLayout newestGoods2;
+        @BindView(R.id.newest_goods_image3)
+        RatioImageView newestGoodsImage3;
+        @BindView(R.id.newest_goods_name3)
+        TextView newestGoodsName3;
+        @BindView(R.id.text3)
+        TextView text3;
+        @BindView(R.id.newest_countDownView3)
+        CountdownView newestCountDownView3;
+        @BindView(R.id.newest_goods3)
+        LinearLayout newestGoods3;
+        @BindView(R.id.text_popular1)
+        TextView textPopular1;
+        @BindView(R.id.line_popular1)
+        View linePopular1;
+        @BindView(R.id.layout_popular1)
+        RelativeLayout layoutPopular1;
+        @BindView(R.id.text_newest1)
+        TextView textNewest1;
+        @BindView(R.id.line_newest1)
+        View lineNewest1;
+        @BindView(R.id.layout_newest1)
+        RelativeLayout layoutNewest1;
+        @BindView(R.id.text_fastest1)
+        TextView textFastest1;
+        @BindView(R.id.line_fastest1)
+        View lineFastest1;
+        @BindView(R.id.layout_fastest1)
+        RelativeLayout layoutFastest1;
+        @BindView(R.id.text_price1)
+        TextView textPrice1;
+        @BindView(R.id.line_price1)
+        View linePrice1;
+        @BindView(R.id.layout_price1)
+        RelativeLayout layoutPrice1;
+        @BindView(R.id.title_bar1)
+        LinearLayout titleBar1;
+
+        IndianaHeaderView(View view) {
+            ButterKnife.bind(this, view);
+            layoutPopular1.setOnClickListener(onClickListener);
+            layoutNewest1.setOnClickListener(onClickListener);
+            layoutFastest1.setOnClickListener(onClickListener);
+            layoutPrice1.setOnClickListener(onClickListener);
+        }
+
+        void bindNewestGoods() {
+            newestGoodsName1.setText("最新揭晓1");
+            newestGoodsName2.setText("最新揭晓2");
+            newestGoodsName3.setText("最新揭晓3");
+            newestGoodsName1.setTag(R.id.newest_countDownView1);
+            newestGoodsName2.setTag(R.id.newest_countDownView2);
+            newestGoodsName3.setTag(R.id.newest_countDownView3);
+            newestCountDownView1.start(10 * 1000);
+            newestCountDownView2.start(20 * 1000);
+            newestCountDownView3.start(30 * 1000);
+
+        }
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.layout_popular1:
+                    presenter.setChoosedTitleBar(TITLE_BAR_popular);
+                    break;
+                case R.id.layout_newest1:
+                    presenter.setChoosedTitleBar(TITLE_BAR_NEWEST);
+                    break;
+                case R.id.layout_fastest1:
+                    presenter.setChoosedTitleBar(TITLE_BAR_FASTEST);
+                    break;
+                case R.id.layout_price1:
+                    presenter.setChoosedTitleBar(TITLE_BAR_PRICE);
+                    break;
+            }
+        }
+    };
 }
--- app/src/main/java/wiki/scene/shop/fragment/mine/MineFragment.java
+++ app/src/main/java/wiki/scene/shop/fragment/mine/MineFragment.java
@@ -11,14 +11,16 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import wiki.scene.shop.R;
 import wiki.scene.shop.activity.LoginActivity;
-import wiki.scene.shop.base.BaseMainFragment;
+import wiki.scene.shop.fragment.mine.mvpview.IMineView;
+import wiki.scene.shop.fragment.mine.presenter.MinePresenter;
+import wiki.scene.shop.mvp.BaseMainMvpFragment;
 
 /**
  * Case By:我的
  * package:wiki.scene.shop.fragment.indiana
  * Author：scene on 2017/6/26 14:13
  */
-public class MineFragment extends BaseMainFragment {
+public class MineFragment extends BaseMainMvpFragment<IMineView, MinePresenter> implements IMineView {
 
     public static MineFragment newInstance() {
         MineFragment fragment = new MineFragment();
@@ -40,8 +42,120 @@ public class MineFragment extends BaseMainFragment {
         super.onLazyInitView(savedInstanceState);
     }
 
-    @OnClick(R.id.btn)
+    @OnClick(R.id.user_avater)
     public void onClickLogin() {
+
+    }
+
+    /**
+     * 充值
+     */
+    @OnClick(R.id.recharge)
+    public void onClickRecharge() {
+        presenter.recharge();
+    }
+
+    /**
+     * 夺宝记录
+     */
+    @OnClick(R.id.indiana_record)
+    public void onClickIndianaRecord() {
+        presenter.recharge();
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+    }
+
+    @Override
+    public MinePresenter initPresenter() {
+        return new MinePresenter(this);
+    }
+
+
+    /**
+     * 登录
+     */
+    @Override
+    public void enterLogin() {
         startActivity(new Intent(_mActivity, LoginActivity.class));
+    }
+
+    /**
+     * 夺宝记录
+     */
+    @Override
+    public void enterIndianaRecord() {
+
+    }
+
+    /**
+     * 中奖记录
+     */
+    @Override
+    public void enterWinRecord() {
+
+    }
+
+    /**
+     * 夺宝攻略
+     */
+    @Override
+    public void enterIndianaRaiders() {
+
+    }
+
+    /**
+     * 晒单
+     */
+    @Override
+    public void enterMineShare() {
+
+    }
+
+    /**
+     * 我的红包
+     */
+    @Override
+    public void enterMineRed() {
+
+    }
+
+    /**
+     * 我的收货地址
+     */
+    @Override
+    public void enterReceiverAddress() {
+
+    }
+
+    /**
+     * 服务中心
+     */
+    @Override
+    public void enterServiceCenter() {
+
+    }
+
+    /**
+     * 分享app
+     */
+    @Override
+    public void enterShareApp() {
+
+    }
+
+    /**
+     * 设置
+     */
+    @Override
+    public void enterSetting() {
+
     }
 }
