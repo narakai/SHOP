5c0f889463c6001892a911d1c57d74e649874844
renjunjia3 <376124750@qq.com>
2017-07-18 17:06:55 +0800

    删除无用的注释

--- app/src/main/java/wiki/scene/shop/MainActivity.java
+++ app/src/main/java/wiki/scene/shop/MainActivity.java
@@ -43,13 +43,11 @@ public class MainActivity extends SupportActivity {
 
     @Override
     public void onBackPressedSupport() {
-        // 对于 4个类别的主Fragment内的回退back逻辑,已经在其onBackPressedSupport里各自处理了
         super.onBackPressedSupport();
     }
 
     @Override
     public FragmentAnimator onCreateFragmentAnimator() {
-        // 设置横向(和安卓4.x动画相同)
         return new DefaultHorizontalAnimator();
     }
 
